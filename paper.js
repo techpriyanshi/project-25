77c6f1601c93ea09d65f44cce68ec4e6d46aa0c2
bunny <82957061+techpriyanshi@users.noreply.github.com>
2021-06-28 15:49:42 +0530

    Update paper.js

--- paper.js
+++ paper.js
@@ -2,15 +2,15 @@ class Paper{
 constructor(){
 var options ={  
 
-restitution: 0,
-friction: 0,
-density: 1.2
+restitution :0,
+friction :0,
+density  :1.2
 
  }
  this.body = Bodies.circle(250,540,20,options);
- this.width = 33;
+ this.width =33;
  this.image = loadImage("paper.png");
- World.add(world,this.body);
+ World.add(world, this.body);
 }
 display(){
     var pos = this.body.position;
